9735c5432da879325631f926bdcd4d9ecc3500da
Henry AT <yiperu@gmail.com>
2013-10-06 00:09:33 -0500

    Modificamos el archivo 'enrutador.js' de tal forma que soporte un parametro mas para que retorne el manejador

--- enrutador.js
+++ enrutador.js
@@ -1,5 +1,6 @@
-function funcionEnrutar(ruta) {
-    console.log("Voy a Rutear algo para " + ruta);
+function funcionEnrutar(arg_manejador,arg_ruta) {
+    console.log("Voy a Rutear algo para " + arg_ruta);
+    return arg_manejador[arg_ruta]();
 }
 
 exports.parametroRutear = funcionEnrutar;
