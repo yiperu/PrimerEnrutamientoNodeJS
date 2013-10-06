28aa9ce05bbd543cfb7e61bbb651e39638c79e5c
Henry AT <yiperu@gmail.com>
2013-10-05 22:39:48 -0500

    Se modifico el archivo 'servidor.js' de tal forma que 1.- Usemos el modulo url, 2.- soporte argumento la funcion iniciar, 3.- captemos en una variable la URl ingresada, 4.- llamemos al modulo creado 'enrutar.js' aprovechando que este entra como argumento de la funcion principal de  'servidor.js'

--- servidor.js
+++ servidor.js
@@ -1,14 +1,21 @@
-var servidor = require("http");
-var url = require("url");
+var variableModuloHttp = require("http");
+var variableModuloUrl = require("url");
 
-function funcionIniciar() {
+function funcionIniciar(argumentoFuncionIniciar) {
+
     function funcionArrancaServidor(requiere, respuesta) {
+
+        var variableRuta = variableModuloUrl.parse(requiere.url).pathname;
+
         console.log("Alguien se ha conectado desde un navegador");
+
+        argumentoFuncionIniciar(variableRuta);
+
         respuesta.writeHead(200,{"Content-Type":"text/html"});
         respuesta.write("<h1>El servido Funciona Correctamente usando Ahora Modulo</h1>");
         respuesta.end("El texto que escribo aqui tambien se muestra en el navegador");
     }
-    servidor.createServer(funcionArrancaServidor).listen(8888);
+    variableModuloHttp.createServer(funcionArrancaServidor).listen(8888);
 }
 
 exports.exportsDeServidor = funcionIniciar;
