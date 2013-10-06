1c9febcafc317ad334ada6602756b1de7b951a39
Henry AT <yiperu@gmail.com>
2013-10-05 23:53:44 -0500

    Se modifica el archivo 'inicio.js' de tal forma que trabaje con el nuevo modulo creado 'peticiones' y creamos un arreglo manejador

--- inicio.js
+++ inicio.js
@@ -1,4 +1,10 @@
 var variableServidor = require("./servidor");
 var variableEnrutador = require("./enrutador");
+var variablePeticiones = require("./peticiones");
+
+var manejador = {};
+manejador['/'] = variablePeticiones.exportsInicioModuloPeticiones;
+manejador['/pagina1'] = variablePeticiones.exportsPagina1ModuloPeticiones;
+manejador['/pagina2'] = variablePeticiones.exportsPagina2ModuloPeticiones;
 
 variableServidor.exportsDeServidor(variableEnrutador.parametroRutear);
